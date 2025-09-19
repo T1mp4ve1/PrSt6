439ec62a043f029e9279f616b54a8443782d6d5f
T1mp4ve1 <pavelwire@outlook.it>
2025-09-19 10:27:16 +0200

    inizio

---
+++ index-js.js
@@ -0,0 +1,19 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGNkMGU2NTZmMzAyMjAwMTUxMDgwY2QiLCJpYXQiOjE3NTgyNjkwMjksImV4cCI6MTc1OTQ3ODYyOX0.heSqHpY9vfaJL_v1tBNf9ra7RGKoiPlI3IZRe_hE5Zo",
+  },
+});
+
+// {
+//     "name": "Nikia 3310", 
+//     "description": "Indestructible cellphone",
+//     "brand": "Nokia"
+//     "imageURL": "https:example.com/3310.jpg",
+//     "price": 99,
+//     "_id": "jdsn48327kf2kmm543lkn3",
+//     "userId": "kdf43kj23489sdf9g9b29r8se",
+//     "createdAt": "2019-07-19T09:32:12.535Z",
+//     "updatedAt": "2019-07-19T09:32:10.535Z",
+//     "__v": 0
+// }
